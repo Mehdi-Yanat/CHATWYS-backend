d13f8f2063ddaf109373dd95df798590e8e08279
mahdi <hamar3106@gmail.com>
2021-11-21 03:40:02 +0100

    adding new features

--- src/db/mongooseDB.js
+++ src/db/mongooseDB.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const connection_url = 'mongodb://localhost:27017/Tinder-app'
+const connection_url = "mongodb+srv://mahdi:31063106r@cluster0.alpim.mongodb.net/Tinder-app?retryWrites=true&w=majority"
+//'mongodb://localhost:27017/Tinder-app'
 //"mongodb+srv://mahdi:31063106r@cluster0.alpim.mongodb.net/Tinder-app?retryWrites=true&w=majority"
 
 const db =  mongoose.connect(connection_url , {
