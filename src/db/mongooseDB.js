77aec9030eb58fd59d35d50782c97e6e04013ab3
mahdi <hamar3106@gmail.com>
2021-12-23 14:25:51 +0100

    fixing db

--- src/db/mongooseDB.js
+++ src/db/mongooseDB.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose')
 
 
 
-const connection_url = "mongodb+srv://mhd:3106@cluster0.kodgr.mongodb.net/Tinder-App?retryWrites=true&w=majority"
+
+const connection_url = "mongodb+srv://mahdi:31063106r@cluster0.alpim.mongodb.net/Tinder-app?retryWrites=true&w=majority"
+//"mongodb+srv://mhd:3106@cluster0.kodgr.mongodb.net/Tinder-App?retryWrites=true&w=majority"
 //"mongodb://localhost:27017/Tinder-app"
-//"mongodb+srv://mahdi:31063106r@cluster0.alpim.mongodb.net/Tinder-app?retryWrites=true&w=majority"
 
 
 const db =  mongoose.connect(connection_url , {
